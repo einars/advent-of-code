c27a2ba4cb2a4171c23353982d7806cb82ec87cc
Einārs Lielmanis <einars@spicausis.lv>
2025-12-07 19:10:11 +0200

    7.2

--- src/aoc_2025/day7.clj
+++ src/aoc_2025/day7.clj
@@ -9,9 +9,12 @@
 (def input-file "resources/2025/day7.txt")
 
 (defn make-beam [m]
-  (prn m)
   (let [s-pos (first (h/find-keys #{\S} m))]
-    (set [(:x s-pos)] )))
+    #{(:x s-pos)}))
+
+(defn make-timeline-beam [m]
+  (let [s-pos (first (h/find-keys #{\S} m))]
+    {(:x s-pos) 1}))
 
 (defn read-playground [f]
   (h/slurp-xy-map f))
@@ -32,13 +35,30 @@
                new-beam (set new-beam)]
            (recur new-beam (inc y-pos))))))))
 
+(defn count-timelines
+  ([m {:keys [y]}] 
+   (loop [beam (make-timeline-beam m), y-pos 1]
+     (if (= y-pos y)
+       beam
+
+       (let [new-beam (reduce (fn [nb [x n-timelines]]
+
+                                (if (= (get m {:y y-pos :x x}) \^)
+                                  (-> nb
+                                    (update (dec x) (fn [old-val] (+ n-timelines (or old-val 0))))
+                                    (update (inc x) (fn [old-val] (+ n-timelines (or old-val 0)))))
+                                  (-> nb
+                                    (update x (fn [old-val] (+ n-timelines (or old-val 0)))))))
+                        {} beam)]
+         (recur new-beam (inc y-pos)))))))
 
 
 (let [[m d] (read-playground sample-file)]
   (count-tachyon-splits m d))
 
 (defn solve-1 
-  ([] (slurp input-file)))
+  ([] (let [[m d] (read-playground input-file)]
+        (count-tachyon-splits m d))))
 
 (defn solve-2
   ([] (slurp input-file)))
