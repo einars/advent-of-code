4cc1143f7412b22602b0fb1c8fefeba8a2d0700f
Einārs Lielmanis <einars@spicausis.lv>
2025-12-07 20:08:04 +0200

    3.2

--- src/aoc_2025/day3.clj
+++ src/aoc_2025/day3.clj
@@ -12,6 +12,15 @@
         m1 (apply max (drop 1 numbers))]
     (+ m1 (* m0 10))))
 
+(defn max-joltage-n [numbers digits]
+  (loop [numbers numbers, remain (dec digits), accu []]
+    (let [m0 (apply max (drop remain (reverse numbers))) 
+          numbers (next (drop-while (complement #{m0}) numbers))]
+      (if (= remain 0)
+        (parse-long (apply str (conj accu m0)))
+        (recur numbers (dec remain) (conj accu m0))))))
+
+
 (defn explode-number-string [s]
   (map (comp parse-long str) s))
 
@@ -21,15 +30,18 @@
   ([] (solve-1 input-file))
   ([f] (reduce + (map (comp max-joltage explode-number-string) (h/slurp-strings f)))))
 
-(defn solve-2
-  ([] (slurp input-file)))
+(defn solve-2 
+  ([] (solve-2 input-file))
+  ([f] (reduce + (map #(max-joltage-n (explode-number-string %) 12) (h/slurp-strings f)))))
 
 (deftest tests []
   (are [x y] (= x y)
     98 (max-joltage (explode-number-string "987654321111111"))
+    98 (max-joltage-n (explode-number-string "987654321111111") 2)
     89 (max-joltage (explode-number-string "811111111111119"))
     78 (max-joltage (explode-number-string "234234234234278"))
     357 (solve-1 "resources/2025/day3.sample.txt")
+    3121910778619 (solve-2 "resources/2025/day3.sample.txt")
     99 (or 99 115)))
 
 (comment
