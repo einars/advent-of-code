14f0daf6c564634e8ba75d7076c5b24359d04b1d
Einārs Lielmanis <einars@spicausis.lv>
2025-12-07 20:42:52 +0200

    6.2

--- src/aoc_2025/.sample.clj
+++ src/aoc_2025/.sample.clj
@@ -4,13 +4,16 @@
    [clojure.string :as str]
    [aoc.helpers :as h]))
 
+(def sample-file "resources/2025/dayX.sample.txt")
 (def input-file "resources/2025/dayX.txt")
 
 (defn solve-1 
-  ([] (slurp input-file)))
+  ([] (solve-1 input-file))
+  ([f] (prn (h/slurp-strings f))))
 
-(defn solve-2
-  ([] (slurp input-file)))
+(defn solve-2 
+  ([] (solve-2 input-file))
+  ([f] (prn (h/slurp-strings f))))
 
 (deftest tests []
   (are [x y] (= x y)
--- src/aoc_2025/day6.clj
+++ src/aoc_2025/day6.clj
@@ -23,18 +23,56 @@
                                (reduce * 1 (map #(nth % n) number-lines))))
                 operations))))
 
-(pt1 (h/slurp-strings sample-file))
+(defn extract-numbers [ss col]
+  ; returns [numbers, op]
+  (let [operation (nth (last ss) col)
+        number-col (map #(nth % col) (butlast ss))]
+    [(parse-long (str/trim (apply str number-col))) operation]))
+
+(defn parse-pt2 [ss]
+  (for [col (range (count (first ss)))]
+    (extract-numbers ss col)))
+
+(defn pt2 [xs]
+  (loop [x (parse-pt2 xs), accu 0M, op nil, grand-total 0]
+    (prn :accu accu :op op :gt grand-total)
+    (if (seq x)
+      (let [[n new-op] (first x)]
+        (cond
+          (nil? n)
+          (recur (rest x) 0 nil (+ grand-total accu))
+
+          (not= new-op \space)
+          (recur (rest x) n new-op grand-total)
+
+          (= op \*)
+          (recur (rest x) (* n accu) op grand-total)
+
+          (= op \+)
+          (recur (rest x) (+ n accu) op grand-total)
+
+          :else
+          (prn :SHOLD-NOT-HAPPEN :op op)
+
+          ))
+      (+ accu grand-total))))
+
+
+
 
 (defn solve-1 
   ([] (solve-1 input-file))
   ([f] (pt1 (h/slurp-strings f))))
 
-(defn solve-2
-  ([] (slurp input-file)))
+(defn solve-2 
+  ([] (solve-2 input-file))
+  ([f] (pt2 (h/slurp-strings f))))
+
 
 (deftest tests []
   (are [x y] (= x y)
-    4277556 (solve-1 sample-file)))
+    4277556 (solve-1 sample-file)
+    3263827 (pt2 (h/slurp-strings sample-file))))
 
 (comment
   (solve-1)
